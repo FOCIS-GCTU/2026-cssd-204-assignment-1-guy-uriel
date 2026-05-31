4a7fbda67336ab3bd230b69ab5593882fee4920f
Victor Doku <2425402390@live.gctu.edu.gh>
2026-05-31 01:29:57 +0100

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Victor Uriel Doku
+# STUDENT ID: 2425402390
+# Email: YOUR_EMAIL@gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Victor Uriel Doku, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+      pass #code goes here
+    # Prints my initials in normal and large stylized form.
+    
+   print()
 
+   print("...VUD")
 
+   print()
 
+   print("...VV        VV........UU        UU.......DDDDDDDDDD  ")
+   print("...VV        VV........UU        UU.......DD       DD ")
+   print("....VV      VV.........UU        UU.......DD        DD")
+   print("....VV      VV.........UU        UU.......DD        DD")
+   print(".....VV    VV..........UU        UU.......DD        DD")
+   print(".....VV    VV..........UU        UU.......DD        DD")
+   print("......VV  VV...........UU        UU.......DD        DD")
+   print("......VV  VV...........UU        UU.......DD       DD ")
+   print(".......VVVV.......**....UUUUUUUUUU...**...DDDDDDDDDD ")
+   print("........VV........**.....UUUUUUUU....**...DDDDDDDDD ")
+
+   print()
 main()
+
+
